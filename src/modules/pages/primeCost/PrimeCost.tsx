de5813234cb711c2ae8cbb15401908e7ae14e14e
jenya-b <evgeny_rab@inbox.ru>
2022-10-26 23:12:48 +0300

    feat: dialog component

--- src/modules/pages/primeCost/PrimeCost.tsx
+++ src/modules/pages/primeCost/PrimeCost.tsx
@@ -1,13 +1,17 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { v4 } from 'uuid';
 import { primeCostColumnNames } from '../../../constants/table';
 import { Main, MainTitle, PrimaryInput } from '../../../styles/components';
 import { useChangeArticleMutation, useGetArticlesQuery } from '../../../utils/api/productApi';
 import { IArticle } from '../../../utils/api/types';
+import { BasicDialog } from '../../components/dialog/Dialog';
 import { BasicTable } from '../../components/table/Table';
 import { StyledTableCell, StyledTableCellColl } from '../../components/table/TableCell';
 
 export const PrimeCostPage = () => {
+  const [isActiveDialog, setActiveDialog] = useState<boolean>(false);
+  const [articlesId, setArticlesId] = useState<string>();
+  const [costPrices, setCostPrices] = useState<number>();
   const { data: articleList, refetch } = useGetArticlesQuery(null);
   const [setArticle, { isSuccess: isSuccessChangeArticle }] = useChangeArticleMutation();
 
@@ -40,17 +44,34 @@ export const PrimeCostPage = () => {
   const onKeyPressHandler = (event: React.KeyboardEvent<HTMLElement>, articlesId: string) => {
     if (event.key === 'Enter') {
       const costPrices = Number((event.currentTarget as HTMLInputElement).value);
-      setArticle([
-        {
-          costPrices,
-          articlesId,
-        },
-      ]);
+      setArticlesId(articlesId);
+      setCostPrices(costPrices);
+      setActiveDialog(true);
     }
+  };
+
+  const closeDialogWindow = () => {
+    setActiveDialog(false);
+  };
+
+  const confirmAction = () => {
+    setArticle([
+      {
+        costPrices,
+        articlesId,
+      },
+    ]);
+    closeDialogWindow();
   };
 
   return (
     <Main>
+      <BasicDialog
+        isActiveDialog={isActiveDialog}
+        handleClose={closeDialogWindow}
+        handleConfirm={confirmAction}
+        desc="Вы действительно хотите внести изменения?"
+      />
       <MainTitle>Себестоимсть</MainTitle>
       <BasicTable renderRow={renderRow} renderColumnNames={renderColumnNames} data={articleList} />
     </Main>
