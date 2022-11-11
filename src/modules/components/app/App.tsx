9cebe84a77ed641b444b60ce0c4e0ead6a8fcd8b
jenya-b <evgeny_rab@inbox.ru>
2022-11-11 19:44:34 +0300

    refactor: files dir

--- src/modules/components/app/App.tsx
+++ src/modules/components/app/App.tsx
@@ -1,25 +1,26 @@
-import { useEffect, Suspense, lazy } from 'react';
+import { useEffect, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '../../../styles/global';
 import { baseTheme } from '../../../styles/theme';
-import { LoginPage } from '../../pages/login/Login';
 import { Loader } from '../loader/Loader';
 import { routerPath } from '../../../constants/routerPath';
 import { useIsInSystemUserQuery } from '../../../services';
 import { useAppDispatch } from '../../../hooks/redux';
 import { LayoutWrapp } from '../layout/LayouWrapp';
 import { setIsActiveUser } from '../../../store/reducers/userSlice';
-
-const AnaliticsPage = lazy(() => import('./../../pages/analitics/Analitics'));
-const ReportsPage = lazy(() => import('./../../pages/reports/Reports'));
-const SettingsPage = lazy(() => import('../../pages/settings/Settings'));
-const PrimeCostPage = lazy(() => import('../../pages/primeCost/PrimeCost'));
-const Signin = lazy(() => import('../../pages/login/signin/Signin'));
-const Registration = lazy(() => import('../../pages/login/registration/Registration'));
-const PasswordRecovery = lazy(() => import('../../pages/login/passwordRecovery/PasswordRecovery'));
-const PasswordReset = lazy(() => import('../../pages/login/passwordReset/PasswordReset'));
-const NotFoundPage = lazy(() => import('../../pages/notFound/NotFound'));
+import {
+  AnaliticsPage,
+  ReportsPage,
+  SettingsPage,
+  PrimeCostPage,
+  Signin,
+  Registration,
+  PasswordRecovery,
+  PasswordReset,
+  NotFoundPage,
+  LoginPage,
+} from './../../pages';
 
 export const App = () => {
   const {
---
+++ src/modules/pages/index.ts
@@ -0,0 +1,25 @@
+import { lazy } from 'react';
+import { LoginPage } from './../pages/login/Login';
+
+const AnaliticsPage = lazy(() => import('./../pages/analitics/Analitics'));
+const ReportsPage = lazy(() => import('./../pages/reports/Reports'));
+const SettingsPage = lazy(() => import('./../pages/settings/Settings'));
+const PrimeCostPage = lazy(() => import('./../pages/primeCost/PrimeCost'));
+const Signin = lazy(() => import('./../pages/login/signin/Signin'));
+const Registration = lazy(() => import('./../pages/login/registration/Registration'));
+const PasswordRecovery = lazy(() => import('./../pages/login/passwordRecovery/PasswordRecovery'));
+const PasswordReset = lazy(() => import('./../pages/login/passwordReset/PasswordReset'));
+const NotFoundPage = lazy(() => import('./../pages/notFound/NotFound'));
+
+export {
+  AnaliticsPage,
+  ReportsPage,
+  SettingsPage,
+  PrimeCostPage,
+  Signin,
+  Registration,
+  PasswordRecovery,
+  PasswordReset,
+  NotFoundPage,
+  LoginPage,
+};
