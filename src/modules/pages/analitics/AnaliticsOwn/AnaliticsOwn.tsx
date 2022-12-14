abc642c1db6473a565cfad94f0f5f350fc1b8c4f
jenya-b <evgeny_rab@inbox.ru>
2022-12-14 22:19:58 +0300

    feat: add tabs container

--- src/modules/pages/analitics/AnaliticsOwn/AnaliticsOwn.tsx
+++ src/modules/pages/analitics/AnaliticsOwn/AnaliticsOwn.tsx
@@ -1,50 +1,12 @@
 import { useEffect, useState } from 'react';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
 import { useFilter } from 'hooks/useFilter';
 import { useGetOwnDataQuery, useGetOwnAnaliticMutation } from 'services';
 import { Filters, Wrapper, Title, ButtonFilter, Diagram, Table } from './AnaliticsOwn.styled';
 import { SmartTable } from 'modules/components/DataGrid/DataGrid';
 import { Loader } from 'modules/components/Loader/Loader';
-import { VerticalBarChart } from 'modules/components/Charts/VerticalBar/VerticalBar';
-import { LineChart } from 'modules/components/Charts/Line/Line';
 import { FilterWeeks } from 'modules/components/Filters/FilterWeeks';
 import { FilterArticles } from 'modules/components/Filters/FilterArticles';
-
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-}
-
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-function a11yProps(index: number) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
-}
+import { OwnDataTabs } from 'modules/components/Tabs/Tabs';
 
 export const AnaliticsOwn = () => {
   const { data: getOwnData, isSuccess, isLoading: isLoadingGetOwnData } = useGetOwnDataQuery(null);
@@ -77,12 +39,6 @@ export const AnaliticsOwn = () => {
     });
   };
 
-  const [value, setValue] = useState(0);
-
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
-
   return (
     <>
       {(isLoadingGetOwnData || isLoadingFetchOwn) && <Loader />}
@@ -100,22 +56,7 @@ export const AnaliticsOwn = () => {
           )}
           <ButtonFilter onClick={updateData}>Обновить</ButtonFilter>
         </Filters>
-        <Diagram>
-          <Box sx={{ width: '100%' }}>
-            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-              <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-                <Tab label="Продажи/возвраты по артикулам" {...a11yProps(0)} />
-                <Tab label="Продажи/возвраты по неделям" {...a11yProps(1)} />
-              </Tabs>
-            </Box>
-            <TabPanel value={value} index={0}>
-              {ownData && <VerticalBarChart mainData={ownData.analyticsDatas} />}
-            </TabPanel>
-            <TabPanel value={value} index={1}>
-              {ownData && <LineChart mainData={ownData.weekAndSums} />}
-            </TabPanel>
-          </Box>
-        </Diagram>
+        <Diagram>{ownData && <OwnDataTabs ownData={ownData} />}</Diagram>
         <Table>
           <SmartTable data={ownData?.analyticsDatas ?? []} />
         </Table>
