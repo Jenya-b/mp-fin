082e20f2af6d5aba16312a95c23509df8826966b
jenya-b <evgeny_rab@inbox.ru>
2022-12-17 18:59:03 +0300

    refactor: percent bar styled

---
+++ src/modules/components/PercentBar/PercentBar.styled.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export interface WidthProps {
+  maxWidth: number;
+}
+
+export const Wrapper = styled.div`
+  width: 100%;
+  border: 1px solid rgba(255, 255, 255, 0.12);
+  height: 26px;
+  overflow: hidden;
+  position: relative;
+  border-radius: 2px;
+`;
+
+export const PercentText = styled.div`
+  width: 100%;
+  display: flex;
+  position: absolute;
+  line-height: 24px;
+  justify-content: center;
+`;
+
+export const ColorBlock = styled.div<WidthProps>`
+  height: 100%;
+  background-color: ${({ maxWidth }) =>
+    maxWidth >= 70 ? '#088208a3' : maxWidth < 70 && maxWidth >= 40 ? '#efbb5aa3' : '#f44336'};
+  max-width: ${({ maxWidth }) => maxWidth}%;
+`;
--- src/modules/components/PercentBar/PercentBar.tsx
+++ src/modules/components/PercentBar/PercentBar.tsx
@@ -1,12 +1,8 @@
-import styled from 'styled-components';
+import { ColorBlock, PercentText, Wrapper } from './PercentBar.styled';
 
 interface PercentBarProps {
   value: number;
   maxValue: number;
-}
-
-interface WidthProps {
-  maxWidth: number;
 }
 
 export const PercentBar = ({ value, maxValue }: PercentBarProps) => {
@@ -19,27 +15,3 @@ export const PercentBar = ({ value, maxValue }: PercentBarProps) => {
     </Wrapper>
   );
 };
-
-const Wrapper = styled.div`
-  width: 100%;
-  border: 1px solid rgba(255, 255, 255, 0.12);
-  height: 26px;
-  overflow: hidden;
-  position: relative;
-  border-radius: 2px;
-`;
-
-const PercentText = styled.div`
-  width: 100%;
-  display: flex;
-  position: absolute;
-  line-height: 24px;
-  justify-content: center;
-`;
-
-const ColorBlock = styled.div<WidthProps>`
-  height: 100%;
-  background-color: ${({ maxWidth }) =>
-    maxWidth >= 70 ? '#088208a3' : maxWidth < 70 && maxWidth >= 40 ? '#efbb5aa3' : '#f44336'};
-  max-width: ${({ maxWidth }) => maxWidth}%;
-`;
