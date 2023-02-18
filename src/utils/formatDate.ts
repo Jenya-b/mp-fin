dac34d76169d1d9f639cc6e78134c18cd9f4e4ae
jenya-b <evgeny_rab@inbox.ru>
2023-02-18 21:22:47 +0300

    refactor: get date functions

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -2,14 +2,26 @@ import { MIN_YEAR } from 'constants/minYear';
 
 export const formatDateISOString = (parameter: string) => new Date(parameter).toISOString();
 
-export const formatDateGeneral = (parameter: string, full = false) => {
-  const date = new Date(parameter);
-
+const getFullDate = (date: Date) => {
   const day = date.getDate() >= 10 ? date.getDate() : `0${date.getDate()}`;
   const month = date.getMonth() >= 9 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`;
   const year = date.getFullYear();
 
+  return { day, month, year };
+};
+
+export const formatDateGeneral = (parameter: string, full = false) => {
+  const date = new Date(parameter);
+  const { day, month, year } = getFullDate(date);
+
   return full ? `${month}.${day}.${year}` : `${day}.${month}`;
+};
+
+export const getDefaultValueByInputDate = () => {
+  const date = new Date();
+  const { day, month, year } = getFullDate(date);
+
+  return `${year}-${month}-${day}`;
 };
 
 export const getThisYear = () => {
@@ -27,14 +39,4 @@ export const getListOfYears = () => {
   }
 
   return listYears;
-};
-
-export const getDefaultValueByInputDate = () => {
-  const date = new Date();
-
-  const day = date.getDate() >= 10 ? date.getDate() : `0${date.getDate()}`;
-  const month = date.getMonth() >= 9 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`;
-  const year = date.getFullYear();
-
-  return `${year}-${month}-${day}`;
 };
