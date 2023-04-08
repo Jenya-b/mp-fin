02a51e2f9c25228f8932106170cd1d9f3c6c0da1
jenya-b <evgeny_rab@inbox.ru>
2023-04-08 13:39:22 +0300

    refactor: separating styles from a component Loader

---
+++ src/modules/components/Loader/Loader.styled.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const LoaderWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgb(0, 0, 0);
+  background-color: rgba(0, 0, 0, 0.7);
+  z-index: 100;
+`;
--- src/modules/components/Loader/Loader.tsx
+++ src/modules/components/Loader/Loader.tsx
@@ -1,6 +1,6 @@
 import Stack from '@mui/material/Stack';
 import CircularProgress from '@mui/material/CircularProgress';
-import styled from 'styled-components';
+import { LoaderWrapper } from './Loader.styled';
 
 export const Loader = () => {
   return (
@@ -13,17 +13,3 @@ export const Loader = () => {
     </LoaderWrapper>
   );
 };
-
-const LoaderWrapper = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: rgb(0, 0, 0);
-  background-color: rgba(0, 0, 0, 0.7);
-  z-index: 100;
-`;
