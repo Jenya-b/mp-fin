5ca9f846eb7a15aa0077cc164413816ad0368ec5
jenya-b <evgeny_rab@inbox.ru>
2023-04-11 21:48:44 +0300

    refactor: add count days constant

--- src/modules/pages/SearchQuery/Table/Table.tsx
+++ src/modules/pages/SearchQuery/Table/Table.tsx
@@ -6,6 +6,8 @@ import { createArray, subtractDate } from 'utils';
 import { TBody } from './TBody/TBody';
 import { cities } from 'constants/cities';
 
+const countDays = 7;
+
 interface TableProps {
   data: IArticleQueries[];
   deleteSavedArticle: (article: string, query: string) => void;
@@ -17,11 +19,11 @@ export const Table = ({ data, deleteSavedArticle }: TableProps) => {
   useEffect(() => {
     if (!data.length) return;
 
-    setHeadData(createArray(7).map((_, index) => subtractDate(data[0].date, index)));
+    setHeadData(createArray(countDays).map((_, index) => subtractDate(data[0].date, index)));
   }, [data]);
 
   return (
-    <StyledTable count={7}>
+    <StyledTable count={countDays}>
       <THead data={headData} cities={cities} />
       {data.map(({ article, parameters }) => (
         <TBody
